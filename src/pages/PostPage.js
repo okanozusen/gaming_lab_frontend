7baa42ae646a83acf8b51fc654cf3346c846f04f
okanozusen <okanozusen@hotmail.com>
2025-02-12 12:07:06 -0500

    Fixed game search and post retrieval on PostsPage

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -15,7 +15,8 @@ function PostsPage() {
     const [selectedGame, setSelectedGame] = useState(null);
     const [gameSuggestions, setGameSuggestions] = useState([]);
     const [replyContent, setReplyContent] = useState({});
-    
+    const [showReplyBox, setShowReplyBox] = useState({});
+
     const [friends, setFriends] = useState({});
 
     useEffect(() => {
