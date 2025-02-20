f19361bce858ec07974df576c8a7318b31e5c331
okanozusen <okanozusen@hotmail.com>
2025-02-20 03:55:31 -0500

    Fix friend button, game search, and post issue

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -60,6 +60,7 @@ function PostsPage() {
     
         const post = {
             username: user.username,
+            profilePic: user.profilePic || "https://placehold.co/50",
             content: newPost.trim(),
             game_id: selectedGame.id, 
             game_name: selectedGame.name || "Unknown Game",
